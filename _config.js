44060c229f6dbe077a7acf3722f679c87f0c3d3e
ezeen <ek.maigua@gmail.com>
2024-09-26 23:52:56 +0300

    Final Commit

--- _config.js
+++ _config.js
@@ -2,8 +2,8 @@ var config = {}
 
 // Update to have your correct username and password
 config.mongoURI = {
-    production: 'mongodb+srv://Cluster97502:8XxcgmGh60DjiJXe@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
-    development: 'mongodb+srv://Cluster97502:8XxcgmGh60DjiJXe@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
-    test: 'mongodb+srv://Cluster97502:8XxcgmGh60DjiJXe@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
+    production: 'mongodb+srv://ezeen:Wh4MpDAc1fxYUygg@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
+    development: 'mongodb+srv://ezeen:Wh4MpDAc1fxYUygg@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
+    test: 'mongodb+srv://ezeen:Wh4MpDAc1fxYUygg@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
 }
 module.exports = config;
---
+++ readme.md
@@ -0,0 +1,4 @@
+Here is my slack channel https://app.slack.com/client/T07NL796K1B/C07NP5Z0Y9H
+here my website url https://git.heroku.com/jenkins-ip1.git
+
+I am having an error when i try to connect to the mongo server using MongoDB Compass i dont know why. Error is: querySrv ENOTFOUND _mongodb._tcp.gallery.wc344.mongodb.net
